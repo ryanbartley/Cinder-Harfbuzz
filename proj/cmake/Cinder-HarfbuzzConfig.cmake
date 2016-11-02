1229f43cf9ba2f3a132485e5dd94d6637a3187ee
Ryan Bartley <ryanbartley@gmail.com>
2016-11-02 16:14:58 -0400

    updated cinder_path lookup

--- proj/cmake/Cinder-HarfbuzzConfig.cmake
+++ proj/cmake/Cinder-HarfbuzzConfig.cmake
@@ -1,7 +1,9 @@
 if( NOT TARGET Cinder-Harfbuzz )
 	
-	get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE )
-  get_filename_component( BLOCK_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
+	if( NOT EXISTS ${CINDER_PATH} )
+		get_filename_component( CINDER_PATH "${CMAKE_CURRENT_LIST_DIR}/../../../.." ABSOLUTE )
+	endif()
+  	get_filename_component( BLOCK_PATH "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE )
 
 	if( NOT TARGET cinder )
 		include( "${CINDER_PATH}/proj/cmake/configure.cmake" )
@@ -12,8 +14,8 @@ if( NOT TARGET Cinder-Harfbuzz )
 		
 	string( TOLOWER "${CINDER_TARGET}" CINDER_TARGET_LOWER )
 	
-  get_filename_component( HARFBUZZ_LIBS_PATH "${BLOCK_PATH}/lib/${CINDER_TARGET_LOWER}" ABSOLUTE )
-  set( Cinder-Harfbuzz_LIBRARIES ${HARFBUZZ_LIBS_PATH}/libharfbuzz.a )
+  	get_filename_component( HARFBUZZ_LIBS_PATH "${BLOCK_PATH}/lib/${CINDER_TARGET_LOWER}" ABSOLUTE )
+  	set( Cinder-Harfbuzz_LIBRARIES ${HARFBUZZ_LIBS_PATH}/libharfbuzz.a )
 	set( Cinder-Harfbuzz_INCLUDES ${BLOCK_PATH}/include/${CINDER_TARGET_LOWER}/harfbuzz ${CINDER_PATH}/include/freetype )
 
 endif()
